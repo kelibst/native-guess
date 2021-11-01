7aff6bb30b5f6325f2bba4093b2b9c3c882f85a1
Keli Booster <kbooster17@gmail.com>
2021-11-01 22:57:06 +0000

    add needed screens

---
+++ screens/GameScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const GameScreen = () => {
+    return (
+        <View>
+            <Text>Welcome to the game Screen</Text>
+        </View>
+    )
+}
+
+export default GameScreen
---
+++ screens/StartScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const StartScreen = () => {
+    return (
+        <View>
+            <Text>Welcome to the start screen</Text>
+        </View>
+    )
+}
+
+export default StartScreen
